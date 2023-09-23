e21da4fc5f4f8a0f00c3c9d1067a0ef3b4681919
ravigala <72736153+ravigala@users.noreply.github.com>
2023-09-24 02:35:23 +0530

    Bot completed

---
+++ README.md
@@ -0,0 +1,15 @@
+# Telegram Bot Project
+
+A simple Telegram bot created using the Telegraf library in Node.js.
+
+
+## Introduction
+
+This project is a basic Telegram bot that responds to various commands and messages from users. It is built using the Telegraf library for Node.js and can serve as a starting point for more advanced bot development.
+
+## Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+- Node.js installed
+- Telegram Bot API Token (you can obtain this by creating a bot on Telegram)
---
+++ index.js
@@ -0,0 +1,16 @@
+const { Telegraf } = require('telegraf')
+const { message } = require('telegraf/filters')
+require('dotenv').config();
+
+const botToken = process.env.BOT_TOKEN;
+
+const bot = new Telegraf(botToken)
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
